99ad582940bcbfcb1c3d6fa5121e3e31adca7694
Christophe Schmit <ge69zat@mytum.de>
2025-07-10 16:41:30 -0400

    push updates

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/arno-lange-freiburg-stadtfuehrungen",
   server: {
     host: "::",
     port: 8080,
